dd8253c32939373841c4b2276b02dde8bcb6fee5
ygeorgaras <ygeorgaras@gmail.com>
2024-11-11 16:52:23 +0900

    Added Gateway And Completed Consumers

---
+++ src/AuctionService/Consumers/BigPlacedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class BigPlacedConsumer : IConsumer<BidPlaced>
+    {
+        private readonly AuctionDbContext _dbContext;
+
+        public BigPlacedConsumer(AuctionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("--> Consuming bid placed");
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if(auction.CurrentHighBid == null 
+                || context.Message.BidStatus.Contains("Accepted") 
+                && context.Message.Amount > auction.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
